672a4007661864ed19ef835f39617a6d37c94aae
Jakob Hollenstein <517680+jkbjh@users.noreply.github.com>
2025-01-15 14:48:32 +0100

    added rendering to test.

--- test_installation.sh
+++ test_installation.sh
@@ -1,4 +1,4 @@
 python3 -m venv /tmp/tmptestvenv
 source /tmp/tmptestvenv/bin/activate
 python3 -m pip install gymnasium-robotics
-python3 -c "import gymnasium as gym;print(gym.make('Walker2d-v4').reset())"
+python3 -c 'import gymnasium as gym;e = gym.make("Walker2d-v4", render_mode="rgb_array"); print(e.reset()); print(e.render().shape); print("SUCCESS")'
