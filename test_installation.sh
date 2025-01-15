d93156148978d50d40c8d4651ef914febf7d5ce6
Jakob Hollenstein <517680+jkbjh@users.noreply.github.com>
2025-01-15 15:01:06 +0100

    updated for EGL use.

--- test_installation.sh
+++ test_installation.sh
@@ -1,4 +1,5 @@
 python3 -m venv /tmp/tmptestvenv
 source /tmp/tmptestvenv/bin/activate
 python3 -m pip install gymnasium-robotics
-python3 -c 'import gymnasium as gym;e = gym.make("Walker2d-v4", render_mode="rgb_array"); print(e.reset()); print(e.render().shape); print("SUCCESS")'
+export MUJOCO_GL=egl
+python3 -c 'import gymnasium as gym, gc;e = gym.make("Walker2d-v4", render_mode="rgb_array"); print(e.reset()); print(e.render().shape); print("SUCCESS");del e; gc.collect()'
